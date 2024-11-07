9dfd3cd5cdebfae7225063ee4e0bddd9ccb27dd1
cqlyj <luoyingjie0721@gmail.com>
2024-11-07 22:20:21 +0800

    add the MyNft contract

---
+++ src/MyNft.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.25;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNft is ERC721, Ownable {
+    uint256 private s_tokenId;
+
+    constructor() ERC721("MyNft", "MNFT") Ownable(msg.sender) {
+        s_tokenId = 0;
+    }
+
+    function mint(address to) external onlyOwner {
+        _safeMint(to, s_tokenId);
+        s_tokenId++;
+    }
+}
